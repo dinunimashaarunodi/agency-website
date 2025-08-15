42322dde216cf6329d5b9a89d237c0f34dd27869
dinunimasha <dinunimashaarunodi@gmail.com>
2025-08-16 01:57:44 +0530

    hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const Hero = () => {
+  return (
+    <div id='hero' className='flex flex-col items-center gap-6 py-20 px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text:white'>
+        <div className='inline-flex items-center gap-2 border border-gray-300 p-1.5 pr-4 rounded-full'>
+            <img src={assets.group_profile} alt="" />
+            <p className='text-xs font-medium'>Trusted by 10k+ peoples</p>
+        </div>
+        <h1 className='text-4xl sm:text-5xl ms:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>Turning imagination into <span className='bg-gradiant-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent'>digital</span> impact.</h1>
+        <p className='text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-lg pb-3'>Creating meaningfull connections and turning big ideas into interactive digitalexperiences.</p>
+    </div>
+  )
+}
+
+export default Hero
