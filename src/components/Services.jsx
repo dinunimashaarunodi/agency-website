8854bea208052526048cdc95208a4d1cf0c4d149
dinunimasha <dinunimashaarunodi@gmail.com>
2025-08-20 07:02:21 +0530

    finishd but cant host due to payement methoed i will do it later

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import assets from '../assets/assets'
 import Title from './Title'
 import ServiceCard from './ServiceCard'
+import  {motion} from 'motion/react'
+
 
 const Services = () => {
 
@@ -32,7 +34,13 @@ const Services = () => {
 
     ]
   return (
-    <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+    <motion.div
+    initial='hidden'
+    whileInView='visible'
+    viewport={{once:true}}
+    transition={{staggerChildren:0.2}}
+    
+    id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
         <img src={assets.bgImage2} alt='' className='absolute -top-110 -left-70 -z-1 dark:hidden'/>
         <Title title='How can we  help' desc='From strategy to execution ,we craft digital solutions that move your business execution,we craft digital solutions that move your business forword.'/>
         <div  className='flex flex-col md:grid grid-cols-2'>
@@ -42,7 +50,7 @@ const Services = () => {
         </div>
 
 
-    </div>
+    </motion.div>
   )
 }
 
